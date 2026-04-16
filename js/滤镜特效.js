973d6943663e25c723b3f57ef52bfd1953cb7f7c
牡丹君 <49619325+rjdsq@users.noreply.github.com>
2026-04-17 02:57:18 +0800

    Update 滤镜特效.js

--- js/滤镜特效.js
+++ js/滤镜特效.js
@@ -1,5 +1,5 @@
 (function() {
     var s = document.createElement('style');
-    s.textContent = ".img-wrap, #v img, #lv img { filter: contrast(1.2) saturate(1.2); transition: filter 0.5s ease; }";
+    s.textContent = ".img-wrap, #v img, #lv img { filter: contrast(1.2) saturate(1.1); transition: filter 0.5s ease; }";
     document.head.appendChild(s);
 })();
